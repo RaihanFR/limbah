ea9d6cc5c7e94d88c8830159c79e85b023dce8d2
RaihanFR <60203848+RaihanFR@users.noreply.github.com>
2020-02-14 10:48:37 +0700

    Update index.php

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -45,7 +45,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item">
-        <a class="nav-link" href="data.php">
+        <a class="nav-link" href="data">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li> 
